e673441048d05985c56d525160012fd064d389a2
Ditarlix <38490670+Ditarlix@users.noreply.github.com>
2018-05-23 10:52:14 +0200

    Update Grille Courbes eliiptiques.py

--- Grille Courbes eliiptiques.py
+++ Grille Courbes eliiptiques.py
@@ -5,7 +5,7 @@ Created on Wed Apr 25 14:01:19 2018
 @author: urand Théophane
 """
 import tkinter as tk
-p = 7
+p = 43
 a, b = 14,31
 class App(tk.Tk):
     def __init__(self):
@@ -13,14 +13,14 @@ class App(tk.Tk):
         self.title("Courbes elliptiques")
         self.can = tk.Canvas(self, width = 600, height = 600)
         self.can.pack()
-        self.can.create_line(0,550, 600, 550, width = 3)
-        self.can.create_line(50,0, 50, 600, width = 3)
+        self.can.create_line(50,550, 550, 550, width = 3)
+        self.can.create_line(50,50, 50, 550, width = 3)
         for i in range (p):
             self.can.create_line(45, 550-i*500/(p-1), 55, 550-i*500/(p-1), width = 2)
-            self.can.create_line(0, 550-i*500/(p-1), 600, 550-i*500/(p-1))
+            self.can.create_line(50, 550-i*500/(p-1), 550, 550-i*500/(p-1))
             self.can.create_text(35, 550-i*500/(p-1), text = str(i))
             self.can.create_line(550-i*500/(p-1), 545, 550-i*500/(p-1), 555, width = 2)
-            self.can.create_line(550-i*500/(p-1), 0, 550-i*500/(p-1), 600)
+            self.can.create_line(550-i*500/(p-1), 50, 550-i*500/(p-1), 550)
             self.can.create_text(550-i*500/(p-1),565, text = str(p-1-i))
 
         
@@ -51,6 +51,17 @@ if __name__=="__main__":
                 Eab.add((x, y))
     for i in Eab:
         app.placePoint(i, "green")
+
+    Eab = set()
+    a = 8
+    b = 7
+    for x in range (p):
+        for y in range (p):
+            if ((y**2-x**3-a*x-b)%p == 0):
+                Eab.add((x, y))
+    for i in Eab:
+        app.placePoint(i, "red")
+        
         
     Eab = set()
     a = 33
