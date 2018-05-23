ce4f7b88d5a564849232e5343fcfb467b8812746
Ditarlix <38490670+Ditarlix@users.noreply.github.com>
2018-05-23 10:41:41 +0200

    Update Grille Courbes eliiptiques.py

--- Grille Courbes eliiptiques.py
+++ Grille Courbes eliiptiques.py
@@ -5,7 +5,7 @@ Created on Wed Apr 25 14:01:19 2018
 @author: urand Théophane
 """
 import tkinter as tk
-p = 37
+p = 7
 a, b = 14,31
 class App(tk.Tk):
     def __init__(self):
@@ -15,18 +15,18 @@ class App(tk.Tk):
         self.can.pack()
         self.can.create_line(0,550, 600, 550, width = 3)
         self.can.create_line(50,0, 50, 600, width = 3)
-        for i in range (37):
-            self.can.create_line(45, 550-i*500/36, 55, 550-i*500/36, width = 2)
-            self.can.create_line(0, 550-i*500/36, 600, 550-i*500/36)
-            self.can.create_text(35, 550-i*500/36, text = str(i))
-            self.can.create_line(550-i*500/36, 545, 550-i*500/36, 555, width = 2)
-            self.can.create_line(550-i*500/36, 0, 550-i*500/36, 600)
-            self.can.create_text(550-i*500/36,565, text = str(36-i))
+        for i in range (p):
+            self.can.create_line(45, 550-i*500/(p-1), 55, 550-i*500/(p-1), width = 2)
+            self.can.create_line(0, 550-i*500/(p-1), 600, 550-i*500/(p-1))
+            self.can.create_text(35, 550-i*500/(p-1), text = str(i))
+            self.can.create_line(550-i*500/(p-1), 545, 550-i*500/(p-1), 555, width = 2)
+            self.can.create_line(550-i*500/(p-1), 0, 550-i*500/(p-1), 600)
+            self.can.create_text(550-i*500/(p-1),565, text = str(p-1-i))
 
         
-    def placePoint(self, p, color):
-        Px = p[0]*500/36 + 50
-        Py = 500 - p[1]*500/36 + 50
+    def placePoint(self, point, color):
+        Px = point[0]*500/(p-1) + 50
+        Py = 500 - point[1]*500/(p-1) + 50
         self.can.create_oval(Px-5, Py-5, Px+5, Py+5, fill = color)
                     
         
@@ -35,9 +35,9 @@ if __name__=="__main__":
     Eab = set()
     a = 14
     b = 7
-    for x in range (37):
-        for y in range (37):
-            if ((y**2-x**3-a*x-b)%37 == 0):
+    for x in range (p):
+        for y in range (p):
+            if ((y**2-x**3-a*x-b)%p == 0):
                 Eab.add((x, y))
     for i in Eab:
         app.placePoint(i, "magenta")
@@ -45,9 +45,9 @@ if __name__=="__main__":
     Eab = set()
     a = 22
     b = 13
-    for x in range (37):
-        for y in range (37):
-            if ((y**2-x**3-a*x-b)%37 == 0):
+    for x in range (p):
+        for y in range (p):
+            if ((y**2-x**3-a*x-b)%p == 0):
                 Eab.add((x, y))
     for i in Eab:
         app.placePoint(i, "green")
@@ -55,9 +55,9 @@ if __name__=="__main__":
     Eab = set()
     a = 33
     b = 17
-    for x in range (37):
-        for y in range (37):
-            if ((y**2-x**3-a*x-b)%37 == 0):
+    for x in range (p):
+        for y in range (p):
+            if ((y**2-x**3-a*x-b)%p == 0):
                 Eab.add((x, y))
     for i in Eab:
         app.placePoint(i, "blue")
