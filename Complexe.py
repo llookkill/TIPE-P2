d8adba58f669b3bbbeb518c80e46f83d6d309544
Lookkill <lookkill.gaming@gmail.com>
2018-05-25 15:04:58 +0200

    Update Complexe.py

--- Complexe.py
+++ Complexe.py
@@ -1,19 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 25 13:48:20 2018
-
-@author: duran
-"""
-
 class Complexe:
     def __init__(self, reel, imag, num):
         self.reel = reel
         self.imag = imag
         self.n = num
         
-    def __mul__(self, compB):
-        return(Complexe)
-  
+        
+    def __add__(self,compB):
+        reel = self.reel + compB.reel
+        imag = self.imag + compB.imag
+        n=None
+        return(Complexe(reel, imag, n))
+    
+    def __mul__(self,compB):
+        reel=self.reel*compB.reel-self.imag*compB.imag
+        imag=self.reel*compB.imag+self.imag*compB.reel
+        n=None
+        return(Complexe(reel,imag,n))
+
+
 k = 0     
 listComp = [None] * 49
 for reel in range (7):
