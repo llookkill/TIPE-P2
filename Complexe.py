aefccf9addcb8df177658b03ffa57b4d1c156e8f
Thomas D <Moneplokii@gmail.com>
2018-05-30 10:08:01 +0200

    Update Complexe.py
    Alternativ

--- Complexe.py
+++ Complexe.py
@@ -4,56 +4,73 @@ class Complexe:
         self.imag = imag
         self.n = num
         
+    def __str__(self):
         
-    def __add__(self,compB):
-        reel = self.reel + compB.reel
-        imag = self.imag + compB.imag
-        n=None
-        return(Complexe(reel, imag, n))
-    
-    def __mul__(self,compB):
-        reel = self.reel*compB.reel-self.imag*compB.imag
-        imag = self.reel*compB.imag+self.imag*compB.reel
-        n = None
-        return(Complexe(reel,imag,n))
-
-    def __rmul__(self,scalaire):
-        reel = self.reel
-        imag = self.imag
-        for k in range(scalaire-1):
-            reel+=self.reel
-            imag+=self.imag
-        n = None
-        return(Complexe(reel,imag,n))
-
-    def visualiser(self):
         if self.reel==0 and self.imag==1:
-            print('i')
-        elif self.reel==0:
-            print(self.imag,'i',sep='')
-        elif self.imag==1:
-            print(self.reel,'+','i',sep='')
+            return ('i')
         elif self.imag==0:
-            print(self.reel)
+            return ("{}".format(self.reel))
+        elif self.reel==0:
+            return ("{}i".format(self.imag))
+        elif self.imag==1:
+            return ("{}+i".format(self.reel))
         elif self.imag<0:
-            print(self.reel,self.imag,'i',sep='')
+            return ("{}{}i".format(self.reel,self.imag))
         else:
-            print(self.reel,'+',self.imag,'i',sep='')
+            return ("{}+{}i".format(self.reel,self.imag))
+        
+    def __add__(self,B):
+        
+        if isinstance(B,float) or isinstance(B,int):
+            reel = self.reel + B
+            imag = self.imag 
+            
+        if isinstance(B,Complexe):
+            reel = self.reel + B.reel
+            imag = self.imag + B.imag
+            
+        return(Complexe(reel, imag, None))
+        
+    def __radd__(self,B):
+        return(self+B)
+        
+    def __neg__(self):
+        
+        return(Complexe(-self.reel,-self.imag,self.n))
+        
+    def __mul__(self,B):
+        
+        if isinstance(self,Complexe) and isinstance(B,Complexe) : #isinstance is a test for class for example : isinstance(A,Complexe) return True if A is an object from the class Complexe.
+            
+            reel = self.reel*B.reel-self.imag*B.imag
+            imag = self.reel*B.imag+self.imag*B.reel
+            return(Complexe(reel,imag,None))
+            
+        if isinstance(self,Complexe) and isinstance(B,int):
+            
+            reel = self.reel*B
+            imag = self.imag*B
+            return(Complexe(reel,imag,None))
+            
+    def __rmul__(self,B):
+        return(self*B)
 
     def __sub__(self,compB):
-        reel = self.reel - compB.reel
-        imag = self.imag - compB.imag
-        n = None
-        return(Complexe(reel,imag,n))
+        return(self + -compB)
 
-    def __pow__(self,puissance):
-        reel = self.reel
-        imag = self.imag
-        for k in range(puissance-1):
-            reel,imag = reel*self.reel-imag*self.imag,reel*self.imag+imag*self.reel
-        n = None
-        return(Complexe(reel,imag,n))
+#    def __pow__(self,puissance):
+#        reel = self.reel
+#        imag = self.imag
+#        for k in range(puissance-1):
+#            reel,imag = reel*self.reel-imag*self.imag,reel*self.imag+imag*self.reel
+#        n = None
+#        return(Complexe(reel,imag,n))
 
+    def __pow__(self,p):
+        prod = 1
+        for i in range(p):
+            prod = prod*self
+        return (prod)
 
 k = 0     
 listComp = [None] * 49
