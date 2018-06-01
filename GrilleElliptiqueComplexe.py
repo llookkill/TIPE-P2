9bed5c168c979c8725a8572c3c528d4e331022e3
Thomas D <Moneplokii@gmail.com>
2018-06-02 00:01:34 +0200

    Update GrilleElliptiqueComplexe.py

--- GrilleElliptiqueComplexe.py
+++ GrilleElliptiqueComplexe.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-c = 5
+from Complex import *
+
+c = 7
 p = c**2
 
 class App(tk.Tk):
@@ -23,99 +25,24 @@ class App(tk.Tk):
         Px = point[0]*500/(p-1) + 50
         Py = 500 - point[1]*500/(p-1) + 50
         self.can.create_oval(Px-5, Py-5, Px+5, Py+5, fill = color)
-                    
+
 def show(a,b, color):
     for i in courbeElliptique[(a,b)]:
         app.placePoint(i, color)
 
-class Complexe:
-    def __init__(self, reel, imag, num):
-        self.reel = reel
-        self.imag = imag
-        self.n = num
-        
-    def __str__(self):
-        
-        if self.reel==0 and self.imag==1:
-            return ('i')
-        elif self.imag==0:
-            return ("{}".format(self.reel))
-        elif self.reel==0:
-            return ("{}i".format(self.imag))
-        elif self.imag==1:
-            return ("{}+i".format(self.reel))
-        elif self.imag<0:
-            return ("{}{}i".format(self.reel,self.imag))
-        else:
-            return ("{}+{}i".format(self.reel,self.imag))
-        
-    def __add__(self,B):
-        
-        if isinstance(B,float) or isinstance(B,int):
-            reel = self.reel + B
-            imag = self.imag 
-            
-        if isinstance(B,Complexe):
-            reel = self.reel + B.reel
-            imag = self.imag + B.imag
-            
-        return(Complexe(reel, imag, None))
-        
-    def __radd__(self,B):
-        return(self+B)
-        
-    def __neg__(self):
-        
-        return(Complexe(-self.reel,-self.imag,self.n))
-        
-    def __mul__(self,B):
-        
-        if isinstance(self,Complexe) and isinstance(B,Complexe) : #isinstance is a test for class for example : isinstance(A,Complexe) return True if A is an object from the class Complexe.
-            
-            reel = self.reel*B.reel-self.imag*B.imag
-            imag = self.reel*B.imag+self.imag*B.reel
-            return(Complexe(reel,imag,None))
-            
-        if isinstance(self,Complexe) and isinstance(B,int):
-            
-            reel = self.reel*B
-            imag = self.imag*B
-            return(Complexe(reel,imag,None))
-            
-    def __rmul__(self,B):
-        return(self*B)
-
-    def __sub__(self,compB):
-        return(self + -compB)
-    def __eq__(self,B):
-        return (self.reel == B.reel and self.imag == B.imag)
-    
-#    def __pow__(self,puissance):
-#        reel = self.reel
-#        imag = self.imag
-#        for k in range(puissance-1):
-#            reel,imag = reel*self.reel-imag*self.imag,reel*self.imag+imag*self.reel
-#        n = None
-#        return(Complexe(reel,imag,n))
-
-    def __pow__(self,p):
-        prod = 1
-        for i in range(p):
-            prod = prod*self
-        return (prod)
-
-k = 0     
-listComp = [None] * c**2
+ 
+listComp = []
 for reel in range (c):
     for imag in range (c):
-        listComp[k] = Complexe(reel, imag, k)
-        k += 1
+        listComp.append(Complexe(reel,imag,c,c*reel + imag))
+
         
 if __name__=="__main__":
     app = App()
     
     courbeElliptique = dict()
     points = list()
+    
     for a in range (p):
         for b in range (p):
             for x in listComp:
@@ -124,10 +51,10 @@ if __name__=="__main__":
                         points.append((x.n, y.n))
             courbeElliptique[(a,b)] = points
             points = list()
-    
+            
+
 #    for a in range (p):
 #        for b in range (p):
 #            show(a, b, "red")
-    show(6, 2, "blue")
-
+    show(5, 4, "red")
     app.mainloop() 
