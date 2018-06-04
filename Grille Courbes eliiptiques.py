34da481dcce2fec0e5305d5998c16de4a0cccf22
Ditarlix <38490670+Ditarlix@users.noreply.github.com>
2018-06-04 22:14:00 +0200

    Update Grille Courbes eliiptiques.py

--- Grille Courbes eliiptiques.py
+++ Grille Courbes eliiptiques.py
@@ -5,15 +5,15 @@ Created on Wed Apr 25 14:01:19 2018
 @author: urand Théophane
 """
 import tkinter as tk
-p = 37
-a, b = 14,31
+
+p = 37 #Numéro du corps 
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Courbes elliptiques")
-        self.can = tk.Canvas(self, width = 600, height = 600)
+        self.can = tk.Canvas(self, width = 600, height = 600)           #Définition de la fenêtre
         self.can.pack()
-        self.can.create_line(50,550, 550, 550, width = 3)
+        self.can.create_line(50,550, 550, 550, width = 3)               #Définition de la grille
         self.can.create_line(50,50, 50, 550, width = 3)
         for i in range (p):
             self.can.create_line(45, 550-i*500/(p-1), 55, 550-i*500/(p-1), width = 2)
@@ -25,11 +25,19 @@ class App(tk.Tk):
 
         
     def placePoint(self, point, color):
+        """Fonction permetant de placer un point dans la grille
+        Paramètres : point-> un tuple correspondant aux coordonnées du point à placer
+                    color -> Couleur avec laquelle on veut afficher le point
+        """
         Px = point[0]*500/(p-1) + 50
         Py = 500 - point[1]*500/(p-1) + 50
         self.can.create_oval(Px-5, Py-5, Px+5, Py+5, fill = color)
                     
 def show(a,b, color):
+    """Fonction permettant de placer tous les points apartenant à une courbe elliptique donnée
+    Paramètres : a, b -> Les paramètres de la courbe
+    color -> La couleur dans la quelle afficher les points de la courbe
+    """
     for i in courbeElliptique[(a,b)]:
         app.placePoint(i, color)
         
@@ -44,11 +52,12 @@ if __name__=="__main__":
                 for y in range (p):
                     if ((y**2-x**3-a*x-b)%p == 0):
                         points.append((x,y))
-            courbeElliptique[(a,b)] = points
+            courbeElliptique[(a,b)] = points  #On créé un dictionnaire avec comme clé les parametres de la courbe et comme valeurs, tous les points vérifiant l'équation de la courbe
             points = list()
             
-    show(1,3, "red")
-    show(9,17, "blue")
-    
+    show(1,1, "red")
+    show(5, 9, "green")
+    show(8, 30, "yellow")
+
 
     app.mainloop() 
