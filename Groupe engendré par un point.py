c66cd9d8e99f31113b2aadfafc1eb833404ab02c
Ditarlix <38490670+Ditarlix@users.noreply.github.com>
2018-06-04 09:56:04 +0200

    Update Groupe engendré par un point.py

--- Groupe engendré par un point.py
+++ Groupe engendré par un point.py
@@ -38,6 +38,76 @@ if __name__=="__main__":
                         except:
                             pFacteur.add(aux)
                             break
+                            
+#V 2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 11:24:16 2018
+
+@author: duran
+"""
+
+#CCCCCCCCCCCCCCCCCCCCCC
+
+# On a des éléments p (points) avec une ordonnée et une abscisse 
+# p : (a, b)
+
+#Courbe elliptique ---> P + P = Q = 2P (selon les régles précédentes)
+
+from Point import*
+
+
+if __name__=="__main__":
+    #On initialise l'élement du groupe (Z/pZ)
+    k = 100
+    
+    #Liste des points qui ont été testés
+    pUtilises = list()
+    
+    #liste des points facteus de p
+    pFacteur = list()
+    
+    #On boucle sur les différentes courbes
+    for a in range (k):
+        for b in range (k):
+            
+            #On créé la courbe
+            El=EllipticCurve(a,b,k)
+            
+            #On enregistre tous les points à coordonnées entieres sur cette courbe
+            El.create_all_point()
+            
+            #On boucle sur tous les points de la courbe
+            for p in El.list_point:
+                #Si on a un point déja utilisé on passe à la suite
+                if (p in pUtilises):
+                    break
+                grP = list()
+                grP.append(p)
+                if(p.y == 0):
+                    grP.append(Inf(p.curve))
+                else:
+                    try :
+                        print(a, " ", b)
+                        aux = p
+                        q = p+p
+                    except:
+                        pFacteur.append(aux)
+                        pUtilises.append(aux)
+                        break
+                    while (q not in grP):
+                        grP.append(q)
+                        try :
+                            print(a, " ", b)
+                            aux = q
+                            q = q+p
+                        except:
+                            pFacteur.append(aux)
+                            pUtilises.append(aux)
+                            break
+                pUtilises.extend(grP)
+                                    
+                                    
                 
                 setG = set(grP)
                 pUtilises.union(setG)
