7bd5fc00d84e1b59cb3b66af445611ea89d2388b
Ditarlix <38490670+Ditarlix@users.noreply.github.com>
2018-06-05 08:49:11 +0200

    Update factorisation.py

--- factorisation.py
+++ factorisation.py
@@ -12,8 +12,9 @@ Created on Wed May 30 11:24:16 2018
 
 #Courbe elliptique ---> P + P = Q = 2P (selon les régles précédentes)
 
-from Point import*
+from Point import*   # Utilisation du module POint, donnant acces aux classes Point, et ElliptiqueCurve
 def pgcd(a,b) :  
+   #Fonction permettant le calcul du PGCD entre deux entiers a et b
    while a%b != 0 : 
       a, b = b, a%b 
    return b
@@ -38,21 +39,27 @@ if __name__=="__main__":
             
             #On boucle sur tous les points de la courbe
             for p in El.list_point:
+               #Si le point p à déja été utilisé, on passe à la suite afin d'accélérer le processus
                 if p not in pUtilises:
-                    grP = list()
+                    grP = list() #Liste définissant le groupe engendré par P
                     grP.append(p)
+                     
+                     #Si l'ordonnée de P est nulle on passe à la suite car le point n'est pas intéréssant.
                     if(p.y == 0):
                         pass
                     else:
+                     #Pour chaque point on essaie de l'additionner à lui même  si une erreur (inverse inexistant) apparait, on sait que ce point à un facteur commun avec le modulo
                         try :
                             aux = p
                             q = p+p
                         except ValueError:
                             pFacteur.add(aux)
-                            fact.add(pgcd(2*aux.y,k))
+                            fact.add(pgcd(2*aux.y,k)) #Cette instruction permet de retrouver le diviseur du modulo qui nous intéresse
                             pUtilises.add(aux)
                             break
+                          #On boucle sur en additionnant p à q en détéctant à chaque fois si une erreur est soulevée
                         while (q not in grP):
+                           
                             grP.append(q)
                             try :
                                 aux = q
@@ -63,4 +70,5 @@ if __name__=="__main__":
                                 pUtilises.add(aux)
                                 break
                     pUtilises = pUtilises.union(set(grP))
+    #On affiche tous les facteur trouvés
     print(fact)                            
